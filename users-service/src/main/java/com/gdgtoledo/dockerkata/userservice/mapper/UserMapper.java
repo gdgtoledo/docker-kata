bedf58ecc6946ffa2be4658d3f7a38670b57e8e5
Manu Garcia <info@developia.info>
2018-06-16 12:05:11 +0200

    Initial Lombok implementation

--- users-service/src/main/java/com/gdgtoledo/dockerkata/userservice/mapper/UserMapper.java
+++ users-service/src/main/java/com/gdgtoledo/dockerkata/userservice/mapper/UserMapper.java
@@ -2,7 +2,13 @@ package com.gdgtoledo.dockerkata.userservice.mapper;
 
 import com.gdgtoledo.dockerkata.userservice.dto.UserDto;
 import com.gdgtoledo.dockerkata.userservice.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserMapper {
 
     public UserDto UserToUserDto(User user) {
--- users-service/src/main/java/com/gdgtoledo/dockerkata/userservice/model/User.java
+++ users-service/src/main/java/com/gdgtoledo/dockerkata/userservice/model/User.java
@@ -1,10 +1,17 @@
 package com.gdgtoledo.dockerkata.userservice.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.Instant;
 
 @Entity
+@Data
+@EqualsAndHashCode
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
